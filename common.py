fa81972041584ce6eae57b8167c0add5d6163511
C. Anderson Miller <candersonmiller@gmail.com>
2009-06-16 18:34:01 -0400

    cleaning rendundant code from main.py, adding dictionaries for month/day functions

--- common.py
+++ common.py
@@ -44,47 +44,34 @@ def feedFormattedDate(dateToFormat):
 
 
 def month(num):
-	if(num == 1):
-		return "Jan"
-	if(num == 2):
-		return "Feb"
-	if(num == 3):
-		return "Mar"
-	if(num == 4):
-		return "Apr"
-	if(num == 5):
-		return "May"
-	if(num == 6):
-		return "Jun"
-	if(num == 7):
-		return "Jul"
-	if(num == 8):
-		return "Aug"
-	if(num == 9):
-		return "Sep"
-	if(num == 10):
-		return "Oct"
-	if(num == 11):
-		return "Nov"
-	if(num == 12):
-		return "Dec"
+	months = {
+		1 : "Jan",
+		2 : "Feb",
+		3 : "Mar",
+		4 : "Apr",
+		5 : "May",
+		6 : "Jun",
+		7 : "Jul",
+		8 : "Aug",
+		9 : "Sep",
+		10 : "Oct",
+		11 : "Nov",
+		12 : "Dec"
+	}
+	return months[num]
 
 
 def day(num):
-	if(num == 0):
-		return "Mon"
-	if(num == 1):
-		return "Tue"
-	if(num == 2):
-		return "Wed"
-	if(num == 3):
-		return "Thu"
-	if(num == 4):
-		return "Fri"
-	if(num == 5):
-		return "Sat"
-	if(num == 6):
-		return "Sun"
+	days = {
+		0 : "Mon",
+		1 : "Tue",
+		2 : "Wed",
+		3 : "Thu",
+		4 : "Fri",
+		5 : "Sat",
+		6 : "Sun"
+	}
+	return days[num]
 
 
 class BlogPost(db.Model):
--- main.py
+++ main.py
@@ -46,56 +46,8 @@ def feedFormattedDate(dateToFormat):
 	zeroDay = ""
 	if(dateToFormat.day < 10):
 		zeroDay = "0"
-	ohhai = ("%s, %s %s%s, %s at %s%s:%s%s:%s%s" % (day(dateToFormat.isoweekday() - 1),month(dateToFormat.month),zeroDay,dateToFormat.day,dateToFormat.year,zeroHour,dateToFormat.hour,zeroMinute,dateToFormat.minute,zeroSecond,dateToFormat.second))
+	ohhai = ("%s, %s %s%s, %s at %s%s:%s%s:%s%s" % (common.day(dateToFormat.isoweekday() - 1),common.month(dateToFormat.month),zeroDay,dateToFormat.day,dateToFormat.year,zeroHour,dateToFormat.hour,zeroMinute,dateToFormat.minute,zeroSecond,dateToFormat.second))
 	return ohhai
-
-
-def month(num):
-	if(num == 1):
-		return "Jan"
-	if(num == 2):
-		return "Feb"
-	if(num == 3):
-		return "Mar"
-	if(num == 4):
-		return "Apr"
-	if(num == 5):
-		return "May"
-	if(num == 6):
-		return "Jun"
-	if(num == 7):
-		return "Jul"
-	if(num == 8):
-		return "Aug"
-	if(num == 9):
-		return "Sep"
-	if(num == 10):
-		return "Oct"
-	if(num == 11):
-		return "Nov"
-	if(num == 12):
-		return "Dec"
-
-
-def day(num):
-	if(num == 0):
-		return "Mon"
-	if(num == 1):
-		return "Tue"
-	if(num == 2):
-		return "Wed"
-	if(num == 3):
-		return "Thu"
-	if(num == 4):
-		return "Fri"
-	if(num == 5):
-		return "Sat"
-	if(num == 6):
-		return "Sun"
-
-
-
-
 
 
 
